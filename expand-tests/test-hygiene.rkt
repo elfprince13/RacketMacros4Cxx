6c3229475fb406e7c0358fa8227ed8685786dbe9
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2015-05-21 14:30:38 -0400

    demo of let*-macro

--- expand-tests/test-hygiene.rkt
+++ expand-tests/test-hygiene.rkt
@@ -1,29 +1,33 @@
 #lang racket
 
-#;(require macro-debugger/stepper)
+(require macro-debugger/stepper)
 (define-namespace-anchor test-anchor)
 (parameterize ([current-namespace 
                 (namespace-anchor->empty-namespace test-anchor)])
   (namespace-require "test-module.rkt")
   (print (namespace-mapped-symbols)) (newline)
-  ;(print (expand/step (datum->syntax #f '(for5 from 1 to 10 in (let ((tmp it) (y 5)) (begin (swap tmp y) (print it) (print tmp) (print y))))))) (newline)
-  (print (expand-to-top-form (datum->syntax #f '(for5 from 1 to 10 in (let ((tmp it) (y 5)) (begin (swap tmp y) (print it) (print tmp) (print y))))))) (newline))
+  (let ([expand-maybe-step-quoted-form 
+         (lambda (quoted-form)
+           (let ([stx (datum->syntax #f quoted-form)])
+             (begin (print (expand/step stx)) (newline))
+             (begin (print (expand-to-top-form stx)) (newline))))])
+    #;(expand-maybe-step-quoted-form '(for5 from 1 to 10 in (let ((tmp it) (y 5)) (begin (swap tmp y) (print it) (print tmp) (print y)))))
+    (expand-maybe-step-quoted-form '(let* ((tmp 1) (j tmp)) (swap tmp j) (print tmp) (print j)))))
 
 
 
 #;(parameterize ([current-namespace 
-                (namespace-anchor->empty-namespace test-anchor)])
-  (namespace-require "test-module.rkt")
-  (print (namespace-mapped-symbols)) (newline)
-  ;(print (expand/step (datum->syntax #f '(for5 from 1 to 10 in (let ((tmp it) (y 5)) (begin (swap tmp y) (print it) (print tmp) (print y))))))) (newline)
-  (print 
-   (expand-to-top-form 
-    (syntax-property
-     (datum->syntax #f 
-                    '(defun (__global__) void kernelTest ((() int argc) (() char **argv)) 
-                       (begin
-                         (@ Loop1d (test-loop) ((i) (0) (argc)) 
-                            (call printf "%s\\n" (* ((+ argv (@ I () () ()))))))
-                         (call printf "done\\n")))) 
-     'init-skeletons-table init-skeletons-table))) 
-  (newline))
+                  (namespace-anchor->empty-namespace test-anchor)])
+    (namespace-require "test-module.rkt")
+    (print (namespace-mapped-symbols)) (newline)
+    (print 
+     (expand-to-top-form 
+      (syntax-property
+       (datum->syntax #f 
+                      '(defun (__global__) void kernelTest ((() int argc) (() char **argv)) 
+                         (begin
+                           (@ Loop1d (test-loop) ((i) (0) (argc)) 
+                              (call printf "%s\\n" (* ((+ argv (@ I () () ()))))))
+                           (call printf "done\\n")))) 
+       'init-skeletons-table init-skeletons-table))) 
+    (newline))
--- expand-tests/test-module.rkt
+++ expand-tests/test-module.rkt
@@ -2,13 +2,60 @@
   ;this file is primarily necessary because I can't figure out how the search path for an inline-module works
   (provide (all-defined-out))
   
+  (require (for-syntax syntax/stx))
+  (require (for-syntax syntax/context))
+  (require (for-syntax racket/syntax))
+  
+  (define-syntax set!
+    (lambda (stx) 
+      (syntax-case stx (set!)
+        [(set! id val) 
+         (begin
+           (print "Beginning set! inspection") (newline)
+           (print #'id) (newline)
+           (print (identifier-binding #'id)) (newline)
+           (if (identifier? #'val)
+               (begin
+                 (print #'val) (newline)
+                 (print (identifier-binding #'val)) (newline))
+               (begin
+                 (print "No second identifier") (newline)))
+           (print "Ending set! inspection") (newline)
+           #'(set!~ id val))])))
+  
+  (define-syntax let*
+    (lambda (stx)
+      (syntax-case stx (let*)
+        [(let* ((id expr)) bodies ...)
+         (let ([intdef (syntax-local-make-definition-context)])
+           (begin
+             (syntax-local-bind-syntaxes (list #'id) #f intdef)
+             (internal-definition-context-seal intdef)
+             (with-syntax
+                 ([(bodies ...) 
+                   (stx-map 
+                    (lambda (body)
+                      (with-syntax ([body body])
+                        (local-expand #'body (build-expand-context 'expression) #f intdef)))
+                    #'(bodies ...))]
+                  [id (internal-definition-context-apply intdef #'id)]
+                  [lambda (identifier-prune-to-source-module #'lambda)]) ; Strip the original racket definitions!
+               #'((lambda (id) bodies ...) expr))))]
+        [(let* ((id expr) (more-ids more-exprs) ...) bodies ...)
+         (with-syntax
+             ([body #'(let* ((more-ids more-exprs) ...) bodies ...)])
+           #'(let* ((id expr)) body))])))
+  
   (define-syntax swap 
     (lambda (stx)
       (syntax-case stx (swap)
         [(swap a b) 
-         #'(let ([tmp a])
-             (set! a b)
-             (set! b tmp))])))
+         (with-syntax
+             ([let* (identifier-prune-to-source-module #'let*)] ; Strip the original racket definitions!
+              [set! (identifier-prune-to-source-module #'set!)])
+           #'(let* ([tmp a])
+               (set! a b)
+               (set! b tmp)))])))
   
   (define-syntax for5 
     (lambda (stx) 
@@ -19,15 +66,15 @@
             (for5 it from low to high in bodies ...)))]
         [(for5 var from low to high in bodies ...)
          (with-syntax 
-             ([local (identifier-prune-to-source-module #'local)] ; These must be rendered inert!
+             ([local (identifier-prune-to-source-module #'local)] ; Strip the original racket definitions!
               [lambda (identifier-prune-to-source-module #'lambda)]
               [define (identifier-prune-to-source-module #'define)])
-         (syntax #;(this is called 2 loops)
-          (local ([define high-value high] 
-                  [define loop 
-                    (lambda (var)
-                      (if (> var high-value) 'done
-                          (begin
-                            bodies ...
-                            (loop (+ var 1)))))])
-            (loop low))))]))))
+           (syntax #;(this is called 2 loops)
+                   (local ([define high-value high] 
+                           [define loop 
+                             (lambda (var)
+                               (if (> var high-value) 'done
+                                   (begin
+                                     bodies ...
+                                     (loop (+ var 1)))))])
+                     (loop low))))]))))
