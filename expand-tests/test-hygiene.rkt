1d5494b4dfe59b590fe4db93ca93212c4406e33f
Thomas "elfprince13" Dickerson <elfprince13@gmail.com>
2015-05-21 12:27:20 -0400

    setup for second test suite :)

--- expand-tests/test-hygiene.rkt
+++ expand-tests/test-hygiene.rkt
@@ -1,7 +1,6 @@
 #lang racket
 
 #;(require macro-debugger/stepper)
-#;(require "test-module.rkt")
 (define-namespace-anchor test-anchor)
 (parameterize ([current-namespace 
                 (namespace-anchor->empty-namespace test-anchor)])
@@ -9,3 +8,22 @@
   (print (namespace-mapped-symbols)) (newline)
   ;(print (expand/step (datum->syntax #f '(for5 from 1 to 10 in (let ((tmp it) (y 5)) (begin (swap tmp y) (print it) (print tmp) (print y))))))) (newline)
   (print (expand-to-top-form (datum->syntax #f '(for5 from 1 to 10 in (let ((tmp it) (y 5)) (begin (swap tmp y) (print it) (print tmp) (print y))))))) (newline))
+
+
+
+#;(parameterize ([current-namespace 
+                (namespace-anchor->empty-namespace test-anchor)])
+  (namespace-require "test-module.rkt")
+  (print (namespace-mapped-symbols)) (newline)
+  ;(print (expand/step (datum->syntax #f '(for5 from 1 to 10 in (let ((tmp it) (y 5)) (begin (swap tmp y) (print it) (print tmp) (print y))))))) (newline)
+  (print 
+   (expand-to-top-form 
+    (syntax-property
+     (datum->syntax #f 
+                    '(defun (__global__) void kernelTest ((() int argc) (() char **argv)) 
+                       (begin
+                         (@ Loop1d (test-loop) ((i) (0) (argc)) 
+                            (call printf "%s\\n" (* ((+ argv (@ I () () ()))))))
+                         (call printf "done\\n")))) 
+     'init-skeletons-table init-skeletons-table))) 
+  (newline))
